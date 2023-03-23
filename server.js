eec4ed45460d8f8f69a7cbd3bf112cca2745193a
SuperSpawn <thearktitan@gmail.com>
2023-03-23 14:50:30 +0200

    upload all

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const port = 5000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
